107c271dd0467f35181bcd5bbe69915746c9bedf
Confucian <confucian.e@gmail.com>
2023-08-05 22:41:30 +0800

    feat: add home button & adjust spacing

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -1,10 +1,24 @@
 'use client'
 
 import { ConnectButton } from "@rainbow-me/rainbowkit"
+import Link from "next/link"
 
 const Bridge = () => {
   return (
-    <ConnectButton />
+    <section>
+      <div className="flex mt-4 justify-center">
+        <div className='mt-2 mb-2 ml-20'>
+          <Link href='/'>
+            <button className='black_btn text-lg'>
+              Home
+            </button>
+          </Link>
+        </div>
+        <div className="mt-2 mb-2 ml-auto mr-20">
+          <ConnectButton />
+        </div>
+      </div>
+    </section>
   )
 }
 
